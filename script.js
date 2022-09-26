92e7332d805f0a949c2c877b5aafeb00a1774536
DEENOluwatobi <shodimutobie@gmail.com>
2022-09-27 00:57:03 +0100

    Basic Calculator App

---
+++ script.js
@@ -0,0 +1,25 @@
+const values = document.getElementById('values');
+const buttons = document.querySelectorAll('button');
+const allClear = document.getElementById('allClear')
+let srnvalues = '';
+
+
+
+
+for(var i=0;i<buttons.length;i++){
+	buttons[i].addEventListener('click',(e) =>{
+		var numberEntered = e.target.innerHTML;
+		if(numberEntered == 'AC'){
+			values.value = '0';
+		}else if(numberEntered == 'DEL'){
+			values.value = values.value.toString().slice(0, -1);
+		
+		}else if(numberEntered == '='){
+			values.value = eval(srnvalues)
+		}else { 
+			srnvalues+=numberEntered;
+			values.value = srnvalues
+		}
+
+	})
+}	
